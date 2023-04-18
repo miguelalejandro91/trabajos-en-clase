6ea468fb77aa77cc6ce967441b23a54e2f90b032
Miguel <130501880+miguelalejandro91@users.noreply.github.com>
2023-04-18 13:17:57 -0500

    Add files via upload

---
+++ ejemplos.c
@@ -0,0 +1,29 @@
+/* construir un programa en c que determine si un numero entero positivo
+ingresado por el usuario es un numero primo o no, utilice el concepto de funcion
+*/
+
+
+#include <stdio.h>
+int esprimo(int num) {
+int i;
+if (num < 2) {
+return 0;
+    }
+for (i = 2; i < num; i++) {
+if (num % i == 0) {
+return 0;
+        }
+    }
+return 1;
+}
+int main() {
+int num;
+printf("Ingrese un numero entero positivo: ");
+scanf("%d", &num);
+if (esprimo(num)) {
+printf("%d es un numero primo\n", num);
+    } else {
+printf("%d no es un numero primo\n", num);
+    }
+return 0;
+}
